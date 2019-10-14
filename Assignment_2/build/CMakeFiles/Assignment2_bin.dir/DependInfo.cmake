7c92c368e4b0629808ae4397d2d2af7c65a52bae
nikki2606 <nikitamj2606@gmail.com>
2019-10-14 01:28:06 -0400

    Added dependecies for glew and glfw

---
+++ Assignment_2/build/CMakeFiles/Assignment2_bin.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/nikita/ICG-projects/Assignment_2/src/Helpers.cpp" "/home/nikita/ICG-projects/Assignment_2/build/CMakeFiles/Assignment2_bin.dir/src/Helpers.cpp.o"
+  "/home/nikita/ICG-projects/Assignment_2/src/main.cpp" "/home/nikita/ICG-projects/Assignment_2/build/CMakeFiles/Assignment2_bin.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "../../ext/eigen"
+  "../../ext/glfw/include"
+  "../../ext/glew/include"
+  "/home/nikita/ICG-projects/ext/glfw/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/nikita/ICG-projects/Assignment_2/build/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  "/home/nikita/ICG-projects/Assignment_2/build/glew/CMakeFiles/glew.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
